ac4737c0fd33b53f8d8b848eefee4a63ec94c003
GregWaller <waller_g@hotmail.com>
2022-01-13 19:40:03 -0500

    Initial Commit, Day 1 -- Gameboard Init
    Baseline game board is established, along with enumeration for contents and the underpinnings of a coordinate lookup.

---
+++ main/PacMan/Assets/Source/GameMaster.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * Primary game-controller component for Pac-Man facsimile.
+ * Provided to Nvizzio Creations for purposes of skill assessment 
+ * 
+ * Author: Greg Waller
+ * Date: 01.13.2022
+ */
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace LongRoadGames.PacMan
+{
+    public class GameMaster : MonoBehaviour
+    {
+        private const int _BOARD_WIDTH = 28;
+        private const int _BOARD_HEIGHT = 31;
+
+        public Tilemap Gameboard;
+
+        private Dictionary<Vector3Int, GameTile> _playArea;
+
+        public Tile DotTile { get; private set; }
+        public Tile PDotTile { get; private set; }
+        public Tile EmptyTile { get; private set; }
+
+        public void Start()
+        {
+            Debug.Assert(Gameboard != null, "CRITICAL ERROR: A tilemap must be provided.");
+
+            _tile_init();
+            _board_init();
+            _board_setup();
+            //_spawn_player();
+        }
+
+        private void _tile_init()
+        {
+            DotTile = Instantiate(Resources.Load<Tile>("Sprites/Dot"));
+            PDotTile = Instantiate(Resources.Load<Tile>("Sprites/PDot"));
+            EmptyTile = Instantiate(Resources.Load<Tile>("Sprites/Empty"));
+        }
+
+        private void _board_init()
+        {
+            _playArea = new Dictionary<Vector3Int, GameTile>();
+
+            for(int y = 0; y < _BOARD_HEIGHT; y++)
+            {
+                for(int x = 0; x < _BOARD_WIDTH; x++)
+                {
+                    Vector3Int pos = new Vector3Int(x, y, 0);
+                    Tile tile = Gameboard.GetTile<Tile>(pos);
+
+                    if (tile == null)
+                    {
+                        Gameboard.SetTile(pos, EmptyTile);
+                    }
+                    else if (tile.name.Contains("wall"))
+                    {
+                        _playArea.Add(pos, new Wall_GameTile(this, pos, tile));
+                    }
+                    else
+                    {
+                        TileState initialState = (TileState)Enum.Parse(typeof(TileState), tile.name);
+                        GameTile gameTile = new GameTile(this, pos, initialState, tile);
+                        _playArea.Add(pos, gameTile);
+                    }
+                }
+            }
+        }
+
+        private void _board_setup()
+        {
+            foreach(GameTile gameTile in _playArea.Values.Where(t => t.CurrentState != TileState.Wall))
+                gameTile.Reset();
+        }
+
+        private Tile _stateMap(TileState state) => state switch
+        {
+            TileState.Empty => EmptyTile,
+            TileState.Dot => DotTile,
+            TileState.PDot => PDotTile,
+            _ => throw new ArgumentOutOfRangeException($"CRITICAL ERROR: No tile was found for the given state: {state}."),
+        };
+
+        public void SetTile(Vector3Int position, TileState state)
+        {
+            Gameboard.SetTile(position, _stateMap(state));
+        }
+
+        private void _spawn_player()
+        {
+
+        }
+    }
+}
---
+++ main/PacMan/Assets/Source/GameTile.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Primary game-controller component for Pac-Man facsimile.
+ * Provided to Nvizzio Creations for purposes of skill assessment 
+ * 
+ * Author: Greg Waller
+ * Date: 01.13.2022
+ */
+
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace LongRoadGames.PacMan
+{
+    public enum TileState
+    {
+        Empty,
+        Wall,
+        Dot,
+        PDot,
+    }
+
+    public class GameTile
+    {
+        public TileState CurrentState { get; private set; }
+        public Tile Tile { get; private set; }
+        public Vector3Int Position { get; private set; }
+
+        protected TileState _originalState;
+        protected GameMaster _master;
+        
+        public GameTile(GameMaster master, Vector3Int position, TileState state, Tile tile)
+        {
+            _master = master;
+
+            Position = position;
+            _originalState = CurrentState = state;
+            Tile = tile;
+        }
+
+        public void SetState(TileState state)
+        {
+            CurrentState = state;
+            _master.SetTile(Position, state);
+        }
+
+        public void Reset()
+        {
+            SetState(_originalState);
+        }
+    }
+
+    public class Wall_GameTile : GameTile
+    {
+        public Wall_GameTile(GameMaster master, Vector3Int position, Tile tile)
+            : base(master, position, TileState.Wall, tile) { }
+    }
+}
